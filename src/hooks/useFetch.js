39bae7770e4870d68e9be6d4d62d2011ca6da40a
arifwp <arifwahyu696@gmail.com>
2024-04-24 11:12:11 +0700

    first commit

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(fetchUserPlaces, initialValue) {
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState(initialValue);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchUserPlaces();
+        setFetchedData(data);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch data" });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchUserPlaces]);
+
+  return {
+    isFetching,
+    fetchedData,
+    setFetchedData,
+    error,
+  };
+}
